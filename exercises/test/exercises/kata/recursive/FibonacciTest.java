87961a5215da9897126bbb350909ddc52436d97b
lajos-fulep <fulep.lajos@gmail.com>
2013-12-12 15:44:26 +0100

    new KATA exercise.

---
+++ exercises/test/exercises/kata/recursive/FibonacciTest.java
@@ -0,0 +1,14 @@
+package exercises.kata.recursive;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FibonacciTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
